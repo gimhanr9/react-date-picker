e77330a8e32d271c0ddd8c100a8cf7a1613298f0
Gimhan Rodrigo <gimhanrg@gmail.com>
2021-07-21 17:28:38 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import DatePicker from "./Components/Date/DatePicker";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { useState } from "react";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFA500",
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+
+  form: {
+    "& > *": {
+      margin: theme.spacing(1),
+
+      width: "50ch",
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [dateRange, setDateRange] = useState("MM/DD/YYYY");
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  function handleDateChange(dateObj) {
+    const start = dateObj.startDate;
+    const end = dateObj.endDate;
+
+    setDateRange(start + " - " + end);
+    setOpen(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Container maxWidth="lg" className={classes.Container}>
+        <form className={classes.form} noValidate autoComplete="off">
+          <ThemeProvider theme={theme}>
+            <TextField
+              id="outlined-basic"
+              className={classes.textField}
+              label="Date"
+              color="primary"
+              size="small"
+              variant="outlined"
+              value={dateRange}
+              onClick={handleOpen}
+            />
+          </ThemeProvider>
+        </form>
+        {open ? (
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8} lg={9}>
+              <DatePicker handleChange={handleDateChange} />
+            </Grid>
+          </Grid>
+        ) : null}
+      </Container>
     </div>
   );
 }
